56cbdf5d438f375640898d98f035c5c3f268b8f2
Lorenzo Maddalena <lorenzo.maddalena1@gmail.com>
2023-06-15 09:56:02 +0000

    added debug elements

--- javascript.js
+++ javascript.js
@@ -1,7 +1,10 @@
+console.log('you can read me ')
+alert('can you read me ')
 const container = document.querySelector('.container')
 
 const div = document.createElement('div')
 div.style.backgroundColor('blue')
+
 
 container.appendChild(div)
 
