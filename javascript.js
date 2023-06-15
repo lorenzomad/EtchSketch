fbfab8c1740535ffd945f0a015f081b744d98690
Lorenzo Maddalena <lorenzo.maddalena1@gmail.com>
2023-06-15 10:01:14 +0000

    updated values

--- javascript.js
+++ javascript.js
@@ -3,8 +3,17 @@ alert('can you read me ')
 const container = document.querySelector('.container')
 
 const div = document.createElement('div')
+div.style.width = '200px'
+div.style.height = '200px'
 div.style.backgroundColor = 'blue'
 
+const ROWS = 64
 
 container.appendChild(div)
 
+
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
